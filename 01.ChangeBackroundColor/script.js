79014fb47188ba73e21eb5f72d240fb5087b510a
kadoubleU <108259380+kadoubleU@users.noreply.github.com>
2023-02-22 21:25:40 +0200

    Add files via upload

---
+++ 01.ChangeBackroundColor/script.js
@@ -0,0 +1,12 @@
+
+function ChangeColor()   
+     {  
+          const setBg = () => {
+               const randomColor = Math.floor(Math.random()*16777215).toString(16);
+               document.body.style.backgroundColor = "#" + randomColor;
+               color.innerHTML = "#" + randomColor;
+             }
+             
+             buttonColor.addEventListener("click", setBg);
+             setBg();
+     } 
